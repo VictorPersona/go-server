52918959edfb2d30f6f37cb7f2775d715cb45722
VictorPersona <gauravtupe00@gmail.com>
2025-04-02 16:03:36 +0530

    add main.go and static HTML files for basic web server setup

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main(){
+	fileServer := http.FileServer(http.Dir("./static"));
+
+	http.Handle("/",fileServer)
+	http.HandleFunc("/form",formHandler)
+	http.HandleFunc("/hello",helloHandler)
+
+	fmt.Printf()
+}
